fbbb760d74e53a7713eb041c3e4298a2c1990050
yusuf-intern <yusuf.sabuwala@openxpert.cloud>
2026-03-12 11:02:59 +0000

    env port

--- server/server.py
+++ server/server.py
@@ -232,4 +232,5 @@ async def ui():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    PORT = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
